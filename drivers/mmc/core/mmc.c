a6782584b29e52047386b1fd763a4c0366b9b2d5
Marcelo Mergulhao <marcelo.mergulhao@phiinnovations.com>
2018-02-23 16:34:17 -0300

    Adicionando modificação para funcionamento de MMC 5.1

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -366,6 +366,17 @@ static int mmc_decode_ext_csd(struct mmc_card *card, u8 *ext_csd)
 	 * are authorized, see JEDEC JESD84-B50 section B.8.
 	 */
 	card->ext_csd.rev = ext_csd[EXT_CSD_REV];
+	//Henrique Kuehne
+	//Retrabalho para funcionar EMMC 5.1
+	//if (card->ext_csd.rev > 6)	
+	if (card->ext_csd.rev > 8) {              // The '6' has to be replaced with '7' For EMMC5.0
+                                                  //  The '6' has to be replaced with '8'  For EMMC5.1
+		pr_err("%s: unrecognised EXT_CSD revision %d\n",
+			mmc_hostname(card->host), card->ext_csd.rev);
+		err = -EINVAL;
+		goto out;
+         }
+	//Fim do Retrabalho
 
 	card->ext_csd.raw_sectors[0] = ext_csd[EXT_CSD_SEC_CNT + 0];
 	card->ext_csd.raw_sectors[1] = ext_csd[EXT_CSD_SEC_CNT + 1];
